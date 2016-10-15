6ec2ac0cd4383637593255af848a1afcc27234e5
aleinad23 <daniela.gomez@utp.edu.co>
2016-10-15 18:34:53 -0500

    Version 1.0

---
+++ taller.py
@@ -0,0 +1,33 @@
+'''
+pantalla: 640x480
+
+jugadores: 1-2
+
+1 PARTE = 40%
+Especificaciones
+1) Laberinto
+2) 2 Elementos que el jugador debe tocar para ganar
+-
+-
+3) Los enemigos (minimo 5), deben proteger esos objetos
+-
+4) Finaliza:
+    a) Jugador muere    D --> Linea de vida
+    b) Jugador adquiere V
+
+***************
+2 PARTE = 40%
+1) Implementar algoritmos de discretizacion
+    -DDA
+    -Bresenham
+
+    +1 nivel adicional
+    +Presentacion
+    +Final Epilogo
+
+********************************
+20% --> video youtube -Nombres integrantes
+                      -Programa, universidad
+                      -Docente
+    --> Repositorio git
+'''
